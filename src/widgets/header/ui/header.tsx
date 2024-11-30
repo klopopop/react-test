4b7dfb9557996f337d2a1ea75cf4c36da314558d
Pavel Sidorov <pvsidorov@gmail.com>
2024-11-30 18:25:36 +0300

    Подсветка пункта меню

--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -1,22 +1,34 @@
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+
 export const Header = () => {
- 
+    const activeClassName = "text-yellow-300 transition-colors"
+    const inactiveClassName = "hover:text-gray-300 transition-colors"
+
     return (
         <header className="bg-zinc-600 text-white">
             <div className="mx-auto px-4 py-4 flex justify-start items-end">
                 <h1 className="text-xl">e-буки :)</h1>
                 <nav className="ml-5">
-                    <Link to="/" className="hover:text-gray-300 transition-colors">
+                    <NavLink
+                        to="/"
+                        className={({ isActive }) =>
+                            isActive ? activeClassName : inactiveClassName
+                        }
+                    >
                         Главная
-                    </Link>
+                    </NavLink>
                 </nav>
                 <nav className="ml-5">
-                    <Link to="/management" className="hover:text-gray-300 transition-colors">
+                    <NavLink
+                        to="/management"
+                        className={({ isActive }) =>
+                            isActive ? activeClassName : inactiveClassName
+                        }
+                    >
                         Управление книгами
-                    </Link>
+                    </NavLink>
                 </nav>
             </div>
         </header>
     )
- 
 }
