58bff057131f0af67034d782e8ffa8df4f08139f
Pavel Sidorov <pvsidorov@gmail.com>
2024-11-30 14:00:16 +0300

    header

--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -2,7 +2,7 @@ export const Header = () => {
     return (
         <header className="bg-zinc-600 text-white">
             <div className="mx-auto px-4 py-4 flex justify-start items-end">
-                <h1 className="text-xl">e-буксы :)</h1>
+                <h1 className="text-xl">e-буки :)</h1>
                 <nav className="ml-5">
                     <a href="/" className="hover:text-gray-300 transition-colors">Главная</a>
                 </nav>
